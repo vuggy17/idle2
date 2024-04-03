fc4d728d58a59f3b118f46aa7b752d768d568d49
vuggy17 <vuggy17@gmail.com>
2024-04-03 13:47:58 +0700

    update implement

--- packages/backend/functions/src/main.ts
+++ packages/backend/functions/src/main.ts
@@ -4,7 +4,7 @@ import { Models } from 'node-appwrite';
 type Context = {
   req: {
     body: Models.User<{}>;
-    method: 'GET' | 'POST' | 'PATCH' | 'PUT';
+    method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
   };
   res: any;
   log: (msg: any) => void;
@@ -16,19 +16,39 @@ type Context = {
 export default async ({ req, res, log }: Context) => {
   const client = new PrismaClient();
 
-  const { email, $id, $createdAt, $updatedAt } = req.body;
-  log('.');
-  const user = await client.user.create({
-    data: {
-      id: $id,
-      email,
-      role: 'USER',
-      createdAt: new Date($createdAt),
-      updatedAt: new Date($updatedAt),
-    },
-  });
+  if (req.method === 'PATCH') {
+    return res.json({
+      data: req,
+    });
+  }
+
+  if (req.method === 'POST') {
+    const { email, $id, $createdAt, $updatedAt } = req.body;
+    const user = await client.user.create({
+      data: {
+        id: $id,
+        email,
+        role: 'USER',
+        createdAt: new Date($createdAt),
+        updatedAt: new Date($updatedAt),
+      },
+    });
+
+    return res.json({
+      message: 'New user created',
+      data: user,
+    });
+  }
+
+  if (req.method === 'DELETE') {
+    log(JSON.stringify(req, null, 4));
+    return res.json({
+      data: req,
+    });
+  }
+
   return res.json({
-    message: 'New user created',
-    data: user,
+    message: 'No action handled',
+    data: null,
   });
 };
